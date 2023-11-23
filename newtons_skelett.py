45dc1e507604b4687d3bc6aa42a5cef851cda338
anilyildizeldiven <72657298+anilyildizeldiven@users.noreply.github.com>
2023-11-23 18:28:05 +0100

    Add files via upload
    initial code bloc for newtons

---
+++ newtons_skelett.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras import optimizers
+
+class MyNewtonOptimizer(optimizers.Optimizer):
+    def __init__(self, learning_rate=0.001, name="MyNewtonOptimizer", **kwargs):
+        """Call super().__init__() and store hyperparameters"""
+        super().__init__(name, **kwargs)
+        self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
+
+
+    @tf.function
+    def _resource_apply_dense(self, grad, var):
+        """Update the variable using Newton's method for one model variable"""
+      
+        
+    def get_config(self):
+        base_config = super().get_config()
+        return {
+            **base_config,
+            "learning_rate": self._serialize_hyperparameter("learning_rate"),
+        }
+
+# Test des Newton-Optimierers
+# Erstellen eines einfachen Modells und Kompilieren mit dem Newton-Optimierer
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, input_shape=(5,), activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+optimizer = MyNewtonOptimizer(learning_rate=0.1)
+model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
+
+# Generierung von Beispiel-Daten
+import numpy as np
+np.random.seed(42)
+x_train = np.random.randn(1000, 5)
+y_train = np.random.randn(1000, 1)
+
+# Training des Modells
+model.fit(x_train, y_train, epochs=5, batch_size=32)
