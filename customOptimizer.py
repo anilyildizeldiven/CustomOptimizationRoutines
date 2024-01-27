6a6a89087dc594b821117204caec07c458709970
anilyildizeldiven <72657298+anilyildizeldiven@users.noreply.github.com>
2024-01-27 21:02:57 +0100

    Add files via upload

--- customOptimizer.py
+++ customOptimizer.py
@@ -11,7 +11,7 @@ from tensorflow.python.keras.optimizer_v2 import optimizer_v2
 
 
 class NewtonOptimizer(optimizer_v2.OptimizerV2):
-    def __init__(self, learning_rate=0.01, name="NewtonOptimizer", **kwargs):
+    def __init__(self, learning_rate=1.0, name="NewtonOptimizer", **kwargs):
         super(NewtonOptimizer, self).__init__(name, **kwargs)
         self._set_hyper("learning_rate", learning_rate)
 
@@ -20,29 +20,30 @@ class NewtonOptimizer(optimizer_v2.OptimizerV2):
             self.add_slot(var, "accumulator")
 
     def _resource_apply_dense(self, grad, var, apply_state=None):
-        with tf.GradientTape() as t2:
-            with tf.GradientTape() as t1:
-                loss = tf.reduce_mean(var**2)
-    
-            g = t1.gradient(loss, var)
-    
-        h = t2.jacobian(g, var)
-    
+        grad_flat = tf.reshape(grad, [-1])
+        
+        a = var
+        v = a.get_shape()
+        loop = v.num_elements()
+        
+        hessian_list = []
+        for i in range(loop):
+            # Berechnung der zweiten Ableitung für den i-ten Parameter
+            second_derivative = tf.gradients(grad_flat[i], var)[0]
+            hessian_list.append(tf.reshape(second_derivative, [-1]))
+        # Zusammenbau der Hesse-Matrix
+        hessian_flat = tf.stack(hessian_list, axis=1)
+        
         # Reshape des Gradienten und der Hesse-Matrix
         n_params = tf.reduce_prod(var.shape)
-        g_vec = tf.reshape(g, [n_params, 1])
-        h_mat = tf.reshape(h, [n_params, n_params])
-    
-        # Truncation der Hesse-Matrix (nur dominante Eigenwerte beibehalten)
-        threshold = 1e-3  # Schwellenwert für Eigenwerte
-        eigenvalues, eigenvectors = tf.linalg.eigh(h_mat)
-        eigenvalues = tf.where(eigenvalues < threshold, threshold, eigenvalues)
-        h_mat_truncated = tf.matmul(tf.matmul(eigenvectors, tf.linalg.diag(eigenvalues)), tf.transpose(eigenvectors))
-    
+        g_vec = tf.reshape(grad_flat, [n_params, 1])
+        h_mat = tf.reshape(hessian_flat, [n_params, n_params])
+
+        tf.print("Hessian Matrix:", h_mat)
         # Durchführung der Hessian-basierten Newton's Methode mit abgeschnittener Hesse-Matrix
         eps = 1e-4
-        eye_eps = tf.eye(h_mat_truncated.shape[0]) * eps
-        update = tf.linalg.solve(h_mat_truncated + eye_eps, g_vec)
+        eye_eps = tf.eye(h_mat.shape[0]) * eps
+        update = tf.linalg.solve(h_mat + eye_eps, g_vec)
         update *= self.learning_rate
         # Aktualisierung des Variablenwertes
         var_update = var - tf.reshape(update, var.shape)
